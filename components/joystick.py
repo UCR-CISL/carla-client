ea1f40422382fb09fe55ebd10d4e81e9f4857f4f
shahdivyank <divyank.shah.2016@gmail.com>
2025-06-03 00:46:02 -0700

    fix script

--- components/joystick.py
+++ components/joystick.py
@@ -1,6 +1,8 @@
 import pygame
+from datetime import datetime
 
 def main():
+    pygame.init()
     pygame.joystick.init()
 
     joystick = pygame.joystick.Joystick(0)
@@ -8,8 +10,16 @@ def main():
 
     numAxes = joystick.get_numaxes()
 
+    
+
     while True:
+        pygame.event.pump()
+
         jsInputs = [float(joystick.get_axis(i)) for i in range(numAxes)]
-        print(jsInputs)
+
+        now = datetime.now()
+        timestamp = now.timestamp()
+        
+        print(f"{timestamp}:\t{jsInputs}")
 
 main()
